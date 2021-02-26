41a5b4718755f685b942abcd274ca42fa65febfd
Maksim Yurau <2004softwaredeveloper@gmail.com>
2021-02-26 17:17:53 +0300

    Added files.

---
+++ app/src/main/java/com/example/snowflakes/SnowflakesActivity.java
@@ -0,0 +1,15 @@
+package com.example.snowflakes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class SnowflakesActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snowflakes);
+    }
+}
---
+++ app/src/main/java/com/example/snowflakes/SnowflakesView.java
@@ -0,0 +1,13 @@
+package com.example.snowflakes;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class SnowflakesView extends View {
+    public SnowflakesView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
